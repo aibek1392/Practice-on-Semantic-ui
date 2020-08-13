653b09e3b3469d245fef4ffc13b3f5df0bd6eeb4
aibek <ozhorov@gmail.com>
2020-08-12 23:48:38 -0400

    useffect

--- src/index.js
+++ src/index.js
@@ -1,32 +1,8 @@
-import React from 'react';
+import React from 'react'
 import ReactDOM from 'react-dom'
-import CommentDetail from './CommentDetail.js'
-import faker from 'faker';
-import ApprovalCard from './ApprovalCard'
+import App from './App'
 
-
-const App = () => {
-    console.log(faker.image.avatar())
-    return (
-        <div className='ui container comments'>
-             <ApprovalCard>
-                 <div>
-                     <h4 style={{color:'red'}}>Warning!<span>⚠️</span></h4>
-                     Are you sure you want this?
-                 </div>
-             </ApprovalCard>
-            <ApprovalCard>
-                <CommentDetail author='Sam' timeAgo='Today at 4:45PM' postComment='What a nice blog!' faker={faker.image.avatar()} />
-            </ApprovalCard>
-            <ApprovalCard>
-                <CommentDetail author='Alex' timeAgo='Today at 2:00AM' postComment='Awesome Blog!' faker={faker.image.avatar()} />
-            </ApprovalCard>
-            <ApprovalCard>
-                <CommentDetail author='Jane' timeAgo='Today at 5:45PM' postComment='Keep it up, love it!' faker={faker.image.avatar()} />
-            </ApprovalCard>
-        </div>
-
-    )
-};
-
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
